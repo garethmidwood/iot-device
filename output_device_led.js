5c70f13959653ebcb03fe0d55f62ef56389c74aa
John Cook <john@johncook.me.uk>
2019-04-23 19:32:38 +0100

    Added method to control LED (neopixel) strips.
    This requires an LED driver available via GET HTTP requests to port 3000.

--- output_device_led.js
+++ output_device_led.js
@@ -3,6 +3,7 @@ class OutputDevice {
     // you can call the Meteor methods from the control centre
     constructor(ddpclient) {
         this.ddpclient = ddpclient;
+        this.http = require('http');
     }
 
     // this is the device type, it should return a string that
@@ -19,17 +20,38 @@ class OutputDevice {
     // this is called when a record is added to the collection
     // item is a json object containing the values in the DB
     recordAdded(item) {
-        console.log('added item', item);
+        this.sendData(
+            item.led + '/' + 
+            item.colours.r + '/' +
+            item.colours.g + '/' +
+            item.colours.b
+        );
     }
 
     // this is called when a record in the collection is updated
     recordChanged(item) {
-        console.log('changed item', item);
     }
 
     // this is called when a record is removed from the collection
     recordRemoved(_id) {
-        console.log('removed item', _id);
+    }
+
+    connectionFail() {
+        this.sendData('fail');
+    }
+
+    connectionAccepted() {
+        this.sendData('connected');
+    }
+
+    sendData(data) {
+        this.http.get(
+            'http://localhost:3000/' + data,
+            (resp) => {
+                resp.on('data', (chunk) => {});
+                resp.on('end', () => {});
+            }
+        ).on('error', (err) => {});
     }
 }
 
